37e3c58e1a87e46e8d13a7e905a6a5ec1d5f2c4b
Richard Staines <richard.staines@livce.com>
2023-11-17 12:46:56 +0000

    test checker again.

--- pySample.py
+++ pySample.py
@@ -2,9 +2,10 @@ import math
 
 test_data = [1, 9, 8, 33]
 
+
 def first_plus_last(data: list)
     return data[0] + data[-1]
     
- print(first_plus_last(test_data))
+print(first_plus_last(test_data))
 
 
